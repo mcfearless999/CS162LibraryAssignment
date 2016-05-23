88f5e59082bb7caa33e4dd71721a3fb96dd50dd3
marceldelisser <djmdjm3@comcast.net>
2016-05-22 18:21:51 -0700

    Users.h commit

--- LibraryProject/LibraryProject/Users.h
+++ LibraryProject/LibraryProject/Users.h
@@ -1,7 +1,33 @@
 
-
+#include "Book.h"
 #ifndef Users_h
 #define Users_h
 
-
+class Users
+{
+private:
+	string firstName_; 
+	string lastName_; 
+        string email_; //did we want to include email?
+	int ID_; 
+	string checkOut; 
+public:
+        User();
+	User(string firstName, string lastName, int ID, string checkout); 
+	void Register(); 
+	void LogOn(); 
+	void CheckOutBooks();
+        void CheckInBooks(); 
+	void AlreadyCheckedOut();
+	
+	string GetUser(); 
+	string GetFirstName(); //not sure if you prefer this or getuser
+	string GetLastName(); //just let me know what you think. 
+	int GetID(); 
+	string GetCheckOut(); 
+	
+	void setFirstName(string firstName); 
+	void setLastName(string lastName); 
+	void setID(int ID); 
+}
 #endif /* Users_h */
