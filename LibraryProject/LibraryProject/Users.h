784136bdb0246b22cf0367e565a919053ec467e5
Chris M <chris@techfocus.net>
2016-05-23 18:59:38 -0700

    making sure my changes worked

--- LibraryProject/LibraryProject/Users.h
+++ LibraryProject/LibraryProject/Users.h
@@ -1,33 +1,41 @@
 
-#include "Book.h"
+#include "Book.h" //I am not sure we need to include
 #ifndef Users_h
 #define Users_h
 
 class Users
 {
 private:
+    
 	string firstName_; 
 	string lastName_; 
-        string email_; //did we want to include email?
+    string email_;
 	int ID_; 
-	string checkOut; 
+	string checkOut;
+    //we might want to have a login field as well
+    string login;
 public:
-        User();
-	User(string firstName, string lastName, int ID, string checkout); 
+    //added static variables
+    static char userFileList[512];
+    static char currentUser[512];
+    
+    Users();
+	Users(string firstName, string lastName, int ID, string checkout);
 	void Register(); 
 	void LogOn(); 
 	void CheckOutBooks();
         void CheckInBooks(); 
 	void AlreadyCheckedOut();
 	
-	string GetUser(); 
-	string GetFirstName(); //not sure if you prefer this or getuser
-	string GetLastName(); //just let me know what you think. 
+	string GetUser();
+    //I think we should have both first and last
+	string GetFirstName();
+    string GetLastName();
 	int GetID(); 
 	string GetCheckOut(); 
 	
 	void setFirstName(string firstName); 
 	void setLastName(string lastName); 
 	void setID(int ID); 
-}
+};
 #endif /* Users_h */
--- LibraryProject/LibraryProject/main.cpp
+++ LibraryProject/LibraryProject/main.cpp
@@ -7,9 +7,85 @@
 //
 
 #include <iostream>
+#include <fstream>
+#include <string>
+#include "Book.h"
+#include "Users.h"
+
+using namespace std;
+
+char printMenu();
+
+
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    string currentUser;
+    char selection = ' ';
+    //loadFile(Book)
+    //loadFile(Users)
+    //currentUser = LoginMenu();
+    //if (currentUser == "XXX") return 0;
+    //
+    
+    for (;;)
+    {
+        if (selection == 'a')
+        {
+
+            
+            selection = ' ';
+        }
+        if (selection == 'b')
+        {
+
+
+            selection = ' ';
+        }
+        if (selection == 'c')
+        {
+
+            selection = ' ';
+        }
+        if (selection == 'd')
+        {
+
+            selection = ' ';
+        }
+        if (selection == 'e'){
+
+
+            selection = ' ';
+            
+        }
+        if (selection == 'f'){
+
+            return 0;
+            
+        }else{
+            selection = printMenu();
+        }
+    
+
+    }
+    
+    
     return 0;
 }
+//this is a generalized menu that would display different options
+char printMenu()
+{
+    char choice = ' ';
+    cout << "Please choose an option" << endl;
+    cout << "a) " << endl;
+    cout << "b) " << endl;
+    cout << "c) " << endl;
+    cout << "d) " << endl;
+    cout << "e) " << endl;
+    cout << "f) Quit" << endl;
+    cout << " -> ";
+    
+    cin >> choice;
+    
+    return choice;
+}
+
